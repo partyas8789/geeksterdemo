0e9cac925764ac4270e55256039eea410d7bafc5
pratyas8789 <kpratyas@gmail.com>
2022-10-28 12:54:52 +0530

    adapter

---
+++ Consumer.java
@@ -0,0 +1,11 @@
+public class Consumer  {
+    INumberProvider provider;
+
+    Consumer(INumberProvider provider) {
+        this.provider = provider;
+    }
+
+    public void advertisementSentTo() {
+        System.out.println(provider.getPhoneNos());
+    }
+}
---
+++ Test.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Test {
+    public static void main(String[] args) {
+        EngineerNumberProvider obj1 = new EngineerNumberProvider();
+        StudentNumberProvider obj2 = new StudentNumberProvider();
+        Consumer consumer1 = new Consumer(obj1);
+        Consumer consumer2 = new Consumer(obj2);
+        consumer1.advertisementSentTo();
+        consumer2.advertisementSentTo();
+    }
+}
